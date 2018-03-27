73c04229a2b168b96f56ec5dafc57071b623446a
Tim Stevens <timstevens@hush.com>
2018-03-27 09:20:32 -0500

    got a little something working

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,69 +1,86 @@
-var question1 = {
-    question: 'Which bass does not belong to the genus, Micropterus?',
-    wrong: ['smallmouth bass','redeye bass','Alabama bass'],
-    right: 'striped bass',
-};
-
-var question2 = {
-    question: 'Which species of fish was introduced by European settlers?',
-    wrong: ['smallmouth bass', 'shoal bass','rainbow trout'],
-    right: ['brown trout'],
-};
-
-var question3 = {
-    question: 'Which species do not qualify for weigh-in in B.A.S.S. sanctioned tournaments?',
-    wrong: ['Alabama bass', 'smallmouth bass','spotted bass'],
-    right: 'striped bass',
-};
-var triviaArray = [question1, question2, question3];
-
 var success = 'Great job, Sport! See if you can get another one right';
 var statCorrect = 0;
 var statWrong = 0;
 
-window.onload = function() {
-    // timer.start();
-    // $("#reset").on("click", timer.reset);  // change this to be done automatically
-    $("#startGame").on("click", timer.start);
-  };
-    var intervalId;
-    var clockRunning = false;
+var trivObject1 = {
+    question: 'Which bass does not belong to the genus, Micropterus?',
+    choices: ['smallmouth bass','redeye bass','Alabama bass','striped bass'],
+    answer: 'striped bass',
+};
 
-    var timer = {
-        time: 0,
-        reset: function() {
-            timer.time = 0;
-            timer.lap = 1;
-            $("#timer").text("00:00");
-            },
-        start: function() {
-            if (!clockRunning) {
-                intervalId = setInterval(timer.count, 1000);
-                clockRunning = true;
-            }
-        },
-        stop: function() {
-            clearInterval(intervalId);
-            clockRunning = false;
-        },
-        count: function() {
-            timer.time++;
-            var converted = timer.timeConverter(timer.time);
-            console.log(converted);
-            $("#timer").text(converted);
-        },
-        timeConverter: function(t) {
-            var minutes = Math.floor(t / 60);
-            var seconds = t - (minutes * 60);
-            if (seconds < 10) {
-                seconds = "0" + seconds;
-            }
-            if (minutes === 0) {
-                minutes = "00";
-            }
-            else if (minutes < 10) {
-                minutes = "0" + minutes;
-            }
-            return minutes + ":" + seconds;
+var trivObject2 = {
+    question: 'Which species of fish was introduced by European settlers?',
+    choices: ['bla bass', 'shoal bass','rainbow trout','brown trout'],
+    answer: ['brown trout'],
+};
+
+var trivObject3 = {
+    question: 'Which species do not qualify in B.A.S.S.-sanctioned tournaments?',
+    choices: ['Alabama bass', 'smallmouth bass','spotted bass','striped bass'],
+    answer: 'striped bass',
+};
+
+var triviaArray = [trivObject1, trivObject2, trivObject3];
+
+var trivQuest = trivObject1.question;
+var trivChoices = trivObject1.choices;
+
+function getQuestion() {
+    $('#question').append(trivQuest);
+    for (var j = 0; j < trivChoices.length; j++) {
+        var choice = $('<span>');
+        choice.attr('id','test');
+        choice.append(trivChoices[j]);
+        $('#choices').append(choice);
+        // $('#answers').append(trivChoices[j]); THIS WORKS
+        // console.log(trivChoices[j]);  THIS WORKS
+        console.log(trivChoices[j]);
+    }
+    
+};
+
+$("#startGame").click(function() {
+    $('#startGame').remove();
+    getQuestion();
+    timer.start();
+    timer.theEnd();
+});
+
+var intervalId;
+var clockRunning = false;
+var timer = {
+    time: 5,
+    start: function() {
+        if (!clockRunning) {
+            intervalId = setInterval(timer.count, 1000);
+            clockRunning = true;
         }
-    };
+    }, 
+    count: function() {
+        timer.time--;
+        nowTime = timer.time;
+        var converted = timer.timeConverter(timer.time);
+        $("#timer").text(converted);
+    },
+    timeConverter: function(t) {
+        var minutes = Math.floor(t / 60);
+        var seconds = t - (minutes * 60);
+        if (seconds < 10) {
+            seconds = "0" + seconds;
+        }
+        if (minutes === 0) {
+            minutes = "00";
+        }
+        else if (minutes < 10) {
+            minutes = "0" + minutes;
+        }
+        return minutes + ":" + seconds;
+    },
+    theEnd: function() {
+        setTimeout(gameOver, 5000);
+        function gameOver() {
+            console.log('game OVER');
+            clearInterval(intervalId);
+        }
+    },
+};
